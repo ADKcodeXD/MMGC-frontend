b91c020f00cdaed29d9b53495d71b350efc228b0
adkcodeMobilePC <790532173@qq.com>
2023-01-26 21:05:55 +0800

    remove: remove useless page or components

--- components/Counter.vue
+++
@@ -1,8 +0,0 @@
-<script setup lang="ts">
-import { ElInputNumber } from 'element-plus'
-const { count } = useCount()
-</script>
-
-<template>
-  <el-input-number v-model="count" :min="1" :max="10" />
-</template>
