4d4b2ba9f1c277ead04ef4dc7cad8e81e714062b
ADKCodeXD <790532173@qq.com>
2023-03-11 11:30:31 +0800

    style: 💄 clean up

---
+++ components/LoadingPage.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="bg-black flex items-center justify-center">
+    <MyCustomLoading />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.bg {
+  width: 100vw;
+  height: 100vh;
+}
+</style>
