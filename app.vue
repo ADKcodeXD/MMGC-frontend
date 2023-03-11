d24487384bf8bf7fa5f86f49f765db525d04bbf4
ADKCodeXD <790532173@qq.com>
2023-03-11 11:28:07 +0800

    feat: 🎸 transition optimize

--- app.vue
+++ app.vue
@@ -1,32 +1,34 @@
 <template>
-  <NuxtPage />
+  <NuxtLayout name="root">
+    <Transition name="page" mode="out-in">
+      <div :key="$route.path">
+        <NuxtPage />
+      </div>
+    </Transition>
+  </NuxtLayout>
 </template>
 
 <script setup>
 import { ID_INJECTION_KEY } from 'element-plus'
-import { storeToRefs } from 'pinia'
 import { useGlobalStore } from './stores/global'
 provide(ID_INJECTION_KEY, {
   prefix: 100,
   current: 0
 })
-const globalState = storeToRefs(useGlobalStore())
-
-const readyState = globalState.documentReadyState
-
-watchEffect(() => {
-  if (process.client) readyState.value = document.readyState
+const globalState = useGlobalStore()
+globalState.setConfig().then(() => {
+  globalState.setCurrentActivity()
 })
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 .page-enter-active,
 .page-leave-active {
   transition: all 0.4s ease;
 }
+
 .page-enter-from,
 .page-leave-to {
-  opacity: 0;
-  filter: blur(1rem);
+  filter: blur(1rem) brightness(0);
 }
 </style>
